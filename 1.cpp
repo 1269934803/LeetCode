4af5a0b804ebbd57bc3cbca56cea60d98dbfb7d0
lvmeijin <lvmeijin@192.168.42.14>
2020-02-26 00:27:17 +0800

    save.cpp

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: 2020年02月26日 星期三 00时16分59秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<algorithm>
+#include<stack>
+#include<vector>
+#include<map>
+#include<queue>
+#include<cmath>
+#include<cstring>
+using namespace std;
+
+int main() {
+	int a = 1;
+	cout << a << endl;
+}
