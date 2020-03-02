9a4fd16f37700602edc50182505a8907d716ea4f
1269934803 <1269934803@qq.com>
2020-03-02 11:46:19 +0800

    LeetCode217.cpp

---
+++ LeetCode217.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: LeetCode217.cpp
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: 2020年03月02日 星期一 11时30分32秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+class Solution{
+public:
+	bool containsDuplicate(vector<int> & nums) {
+		unorder_set<int>s;
+		for (int i = 0; i < nums.size(); i++) {
+			if (s.find(nums[i]) != s.end()) return true;
+			s.insert(nums[i]);
+		}
+		return false;
+	}
+};
